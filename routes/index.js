46d2aad7a2abc522e15019f6765643e11527a911
Shyla <Shyla.Grantmyre@MyGeorgian.ca>
2019-06-23 17:30:37 -0400

    added contact, about and other view files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+/* GET about page*/
+router.get('/about', function(req, res, next){
+  res.render('about');
+});
+
+/* GET projects page*/
+router.get('/projects', function(req, res, next){
+  res.render('projects');
+});
+
+/* GET services page*/
+router.get('/services', function(req, res, next){
+  res.render('services');
+});
+
+/* GET contact page*/
+router.get('/contact', function(req, res, next){
+  res.render('contact');
+});
+
 
 module.exports = router;
