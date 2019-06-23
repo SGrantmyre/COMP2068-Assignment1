d3c74ee462c7f1e4dca8a7e471be2d5ef767e069
Shyla <Shyla.Grantmyre@MyGeorgian.ca>
2019-06-23 09:59:06 -0400

    no mongoose, added pug again?

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sgrantmyre:Monkey90@comp2068-shyla-ggvhb.mongodb.net/test?retryWrites=true&w=majority', {
-  useNewUrlParser: true
-});
-
-var db = mongoose.connection;
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
